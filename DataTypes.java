af84041f7ab54c5cf9e50f0506ff9130c88623be
Sai Kiran Battula <bvn.saikiran@gmail.com>
2025-06-27 13:55:29 +0530

    Declaring and displaying all Data types.

---
+++ DataTypes.java
@@ -0,0 +1,35 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        // Integer Type
+        byte myByte = 100;
+        short myShort = 5000;
+        int myInt = 100000;
+        long myLong = 15000000000L;
+
+        // Floating-point Type
+        float myFloat = 5.75f;
+        double myDouble = 19.99d;
+
+        // Character Type
+        char myChar = 'A';
+
+        // Boolean Type
+        boolean myBool = true;
+
+        //-----------------------------------------------------------//
+
+        // Displaying all values
+        System.out.println("byte: " + myByte);
+        System.out.println("short: " + myShort);
+        System.out.println("int: " + myInt);
+        System.out.println("long: " + myLong);
+
+        System.out.println("float: " + myFloat);
+        System.out.println("double: " + myDouble);
+
+        System.out.println("char: " + myChar);
+
+        System.out.println("boolean: " + myBool);
+    }
+}
