f8d398aa993cf348798a8edbe1ab2c76a0609175
Sai Kiran Battula <bvn.saikiran@gmail.com>
2025-06-27 16:43:38 +0530

    Type conversions and casting

---
+++ Java005_TypeConversion.java
@@ -0,0 +1,25 @@
+public class Java005_TypeConversion {
+    public static void main(String[] args) {
+
+        // Widening conversion (implicit casting)
+        int myInt = 9;
+        double myDouble = myInt;
+        System.out.println("int: " + myInt);
+        System.out.println("double after widening: " + myDouble);
+
+        // Narrowing conversion (explicit casting)
+        double anotherDouble = 9.78;
+        int anotherInt = (int) anotherDouble;      // Manual casting: double to int
+        System.out.println("double: " + anotherDouble);
+        System.out.println("int after narrowing: " + anotherInt);
+
+        // Overflow example
+        byte smallByte = (byte) 200;           // byte range is -128 to 127
+        System.out.println("Overflow example: " + smallByte);
+
+        // Character to int conversion
+        char myChar = 'A';
+        int charToInt = myChar;
+        System.out.println("char 'A' as int: " + charToInt);
+    }
+}
