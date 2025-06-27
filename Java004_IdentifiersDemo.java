9c78470cb957cf3a9b20f9fabbe30427589f5535
Sai Kiran Battula <bvn.saikiran@gmail.com>
2025-06-27 16:18:56 +0530

    Renamed the files

--- Java004_IdentifiersDemo.java
+++ Java004_IdentifiersDemo.java
@@ -1,2 +1,23 @@
 public class Java004_IdentifiersDemo {
+    public static void main(String[] args) {
+
+        // Valid Identifiers
+        int age = 25;
+        double $salary = 50000.50;
+        String _message = "Hello";
+        int numberOfStudents = 30;
+
+        // Invalid Identifiers
+        // int 2times = 2;             Error: Starts with number
+        // double my-score = 90;       Error: Contains hyphen
+        // String class = "OOP";       Error: Reserved
+
+        // Invalid Identifiers are commented to avoid errors in the code
+
+        System.out.println("Valid Identifiers:");
+        System.out.println("age: " + age);
+        System.out.println("$salary: " + $salary);
+        System.out.println("_message: " + _message);
+        System.out.println("numberOfStudents: " + numberOfStudents);
+    }
 }
